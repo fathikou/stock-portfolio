5822f73f3a24c516229a45485e3e875f7404cfca
Charlie Cojab <c-cojab-s@hotmail.com>
2021-11-15 21:25:36 -0500

    added tests to portfolio

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,3 +1,4 @@
+import pytest
 from portfolio import Portfolio
 
 def test_empty_portfolio():
@@ -14,3 +15,8 @@ def test_buy_two_stocks():
     p.buy("IBM", 100, 176.48)
     p.buy("HPQ", 100, 36.15)
     assert p.cost() == 21263.0
+
+def test_not_enough_arguments_to_buy():
+    p = Portfolio()
+    with pytest.raises(TypeError):
+        p.buy("IBM")
