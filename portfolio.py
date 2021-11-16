05dda58baa1b9784319885f1e0d06671aaf8424f
fathikou <35569266+fathikou@users.noreply.github.com>
2021-11-15 20:47:40 -0500

    fix
    fix

--- portfolio.py
+++ portfolio.py
@@ -3,7 +3,7 @@ class Portfolio:
     def __init__(self):
         self._stocks = []
     def buy(self, stock_name, stock_quantity, stock_price):
-        self._stocks.append(stock_name, stock_quantity, stock_price)
+        self._stocks.append((stock_name, stock_quantity, stock_price))
     def cost(self):
         return sum(
             stock_quantity * stock_price for stock_name, stock_quantity, stock_price in self._stocks
